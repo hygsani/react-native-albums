e68672995e9f219457ca48402d59bd83c9581743
hygsani <hygsani@hygsanis-MacBook-Pro.local>
2018-08-25 16:05:47 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+    <Text>lorem ipsum</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
