a3c40166885ab0214489ef106c20078e3e0f8619
hygsani <hygsani@hygsanis-MacBook-Pro.local>
2018-08-25 18:16:27 +0700

    add style to the header wrapper (view component)

--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,14 @@ const Header = () => {
 
 const styles = {
     viewStyle: {
-        backgroundColor: '#f8f8f8'
+        backgroundColor: '#f8f8f8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2
     },
     textStyle: {
         fontSize: 20
